6a3cd1629e037d9d8ec0958daa23b0714570085b
Graham <janus-git@exmosis.net>
2022-02-01 12:24:15 +0000

    Adds image optimisation for pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function(eleventyConfig) {
+
+    const path = require('path');
+
+    const nhp = require('node-html-parser');
     
+    const Image = require("@11ty/eleventy-img");
+
     const markdownIt = require('markdown-it');
     const markdownItOptions = {
         html: true,
@@ -33,7 +39,86 @@ module.exports = function(eleventyConfig) {
     });
     
     eleventyConfig.addPassthroughCopy('assets');
+    // eleventyConfig.addPassthroughCopy('notes/**/*.{jpg,jpeg,png,gif}');
+
+    const pageAssetsPlugin = require('eleventy-plugin-page-assets');
+
+    
+    eleventyConfig.addPlugin(pageAssetsPlugin, {
+        mode: "parse",
+        postsMatching: "*.md",
+        assetsMatching: ".jpg",
+        hashAssets: false,
+        // recursive: true,
+    });
+
+
     eleventyConfig.setUseGitIgnore(false);
+
+    // parse images found in Markdown, based on code at
+    // https://gist.github.com/Alexs7zzh/d92ae991ad05ed585d072074ea527b5c
+    // if (process.env.ELEVENTY_ENV) {
+        
+    eleventyConfig.addTransform('transform', (content, outputPath) => {
+
+        if (outputPath && outputPath.endsWith('.html')) {
+
+            let outputPathDir = path.dirname(outputPath)
+            // strip first directory, ie "_site"
+            let relativePath = outputPathDir.substring(outputPathDir.indexOf('/') + 1)
+            // strip second directory, ie "notes"
+            let imgWwwPath = relativePath.substring(outputPathDir.indexOf('/') + 1)
+
+            let document = nhp.parse(content)
+
+            const options = {
+                widths: [640, 1024, 1280, 1600],
+                sizes: '(min-width: 1000px) 50vw, 100vw', // your responsive sizes here
+                formats: ['webp', 'jpeg'],
+                urlPath: '/' + imgWwwPath,
+                outputDir: outputPathDir
+            }
+            
+            // const images = [...document.querySelectorAll('figure img')]
+            const images = [...document.querySelectorAll('img')]
+    
+            images.forEach((i, index) => {
+
+                if (i.getAttribute('src').substring(0, 7) != '/assets' && 
+                    i.getAttribute('src').substring(0, 7) != 'http://' && 
+                    i.getAttribute('src').substring(0, 8) != 'https://') {
+
+                    const src = './' + relativePath + '/' + i.getAttribute('src')
+
+                    const meta = Image.statsSync(src, options)
+                    const last = meta.jpeg[meta.jpeg.length - 1]
+                    if (last.width < 500) return
+                
+                    Image(src, options)
+                    i.setAttribute('width', last.width)
+                    i.setAttribute('height', last.height)
+                    if (index !== 0) {
+                        i.setAttribute('loading', 'lazy')
+                        i.setAttribute('decoding', 'async')
+                    }
+
+                    i.replaceWith(`
+                    <picture class="optimised">
+                        <source type="image/webp" sizes="${options.sizes}" srcset="${meta.webp.map(p => p.srcset).join(', ')}">
+                        <source type="image/jpeg" sizes="${options.sizes}" srcset="${meta.jpeg.map(p => p.srcset).join(', ')}">
+                        ${i.outerHTML}
+                    </picture>`);
+                }
+            })
+
+            return `<!DOCTYPE html>${document.outerHTML}`
+        }
+
+        return content
+        })
+    // }
+
+
 
     return {
         dir: {
